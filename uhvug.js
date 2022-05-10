dc205c550d45552842b80ff8ba31455043b11530
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2022-05-10 08:29:05 -0500

    Update uhvug.js

--- uhvug.js
+++ uhvug.js
@@ -1,71 +1,63 @@
-// do not remove the style thing below -- it is essential
-let style = document.createElement("style")
-// basically creating all elements + adding id's
-let title = document.createElement("h1")
-let otext = document.createElement("p")
-otext.id = "otext"
-title.id = "title"
-let btn1 = document.createElement("div")
-btn1.id = "btn1"
-let btn2 = document.createElement("div")
-btn2.id = "btn2"
-let btn3 = document.createElement("div")
-btn3.id = "btn3"
-let btn4 = document.createElement("div")
-btn4.id = "btn4"
-let backbtn = document.createElement("button")
-backbtn.id = "backbtn"
-otext.style.display = "none"
-let author = document.createElement("h2")
-author.id = "author"
-document.head.appendChild(style)
-let mainframe = document.createElement("div");
-let logo = document.createElement("img")
-mainframe.appendChild(logo)
-mainframe.id = "mainframe"
-// appending all elements
-mainframe.id = "main"
-document.body.appendChild(mainframe)
-mainframe.append(title)
-mainframe.append(backbtn)
-mainframe.append(author)
-mainframe.appendChild(otext)
-logo.src = "";
-// appending all elements
-mainframe.id = "main"
-document.body.appendChild(mainframe)
-mainframe.append(title)
-mainframe.append(backbtn)
-mainframe.append(author)
-mainframe.appendChild(otext)
-mainframe.append(btn1)
-mainframe.append(btn2)
-mainframe.append(btn3)
-mainframe.append(btn4)
-backbtn.textContent = "Go back"
-backbtn.style.display = "none"
+let style = document.createElement("style");
+let title = document.createElement("h1");
+let otext = document.createElement("p");
+otext.id = "otext";
+title.id = "title";
+let btn1 = document.createElement("div");
+btn1.id = "btn1";
+let btn2 = document.createElement("div");
+btn2.id = "btn2";
+let btn3 = document.createElement("div");
+btn3.id = "btn3";
+let btn4 = document.createElement("div");
+btn4.id = "btn4";
+let backbtn = document.createElement("button");
+backbtn.id = "backbtn";
+otext.style.display = "none";
+let undertitle = document.createElement("h2");
+undertitle.id = "undertitle";
+document.head.appendChild(style);
+let frame = document.createElement("div");
+let icon = document.createElement("img");
+frame.appendChild(logo);
+frame.id = "frame";
+frame.id = "main";
+document.body.appendChild(frame);
+frame.append(title);
+frame.append(backbtn);
+frame.append(undertitle);
+frame.appendChild(otext);
+icon.src = "";
+frame.id = "main";
+document.body.appendChild(frame);
+frame.append(title);
+frame.append(backbtn);
+frame.append(undertitle);
+frame.appendChild(otext);
+frame.append(btn1);
+frame.append(btn2);
+frame.append(btn3);
+frame.append(btn4);
+backbtn.textContent = "Go back";
+backbtn.style.display = "none";
 
-// button text
-
-title.textContent = "Title"
-author.textContent = "Under"
-btn1.textContent = ""
-btn2.textContent = ""
-btn3.textContent = ""
-btn4.textContent = ""
-otext.style.display = "none"
-
-// when button is clicked
+title.textContent = "Title";
+undertitle.textContent = "Under";
+btn1.textContent = "Games";
+btn2.textContent = "Bookmarklets";
+btn3.textContent = "About";
+btn4.textContent = "Legal";
+otext.style.display = "none";
 
 btn1.onclick(function btn1() {
 
-}
+};
 btn2.onclick = function btn2() {
 
-}
+};
 btn3.onclick = function btn3() {
 
-}
+};
 btn4.onclick = function btn4() {
 
-}
+};
