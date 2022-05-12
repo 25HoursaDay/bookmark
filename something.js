79e21ae71b5f225134dfe6da45fd22c62c997bb7
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2022-05-12 10:42:14 -0500

    Update something.js

--- something.js
+++ something.js
@@ -1,14 +1,16 @@
 let main = document.createElement("div");
 let gui = document.createElement("style");
-let x = document.createElement("");
-let x = document.createElement("");
-let x = document.createElement("");
-let x = document.createElement("");
-let x = document.createElement("");
-let x = document.createElement("");
+let exit = document.createElement("button");
+let logo = document.createElement("image");
+
+let title = document.createElement("h1");
+let titleA = document.createElement("a");
+let heading = document.createElement("h2");
+let headingA = document.createElement("a");
+
 let btn1 = document.createElement("button");
 let btn2 = document.createElement("button");
 let btn3 = document.createElement("button");
 let btn4 = document.createElement("button");
-let x = document.createElement("");
-let x = document.createElement("");
+let output = document.createElement("p");
+let scripting = document.createElement("script");
