7663e0b51b11867585771902b3fef08f5dc9a39e
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2022-05-12 11:14:05 -0500

    Update something.js

--- something.js
+++ something.js
@@ -1,17 +1,35 @@
+// creating elements 
 let main = document.createElement("div");
-let forcss = document.createElement("head");
-let gui = document.createElement("style");
+let headcss = document.createElement("head");
+let style = document.createElement("style");
 let exit = document.createElement("button");
 let logo = document.createElement("image");
-
 let title = document.createElement("h1");
 let titleA = document.createElement("a");
 let heading = document.createElement("h2");
 let headingA = document.createElement("a");
-
 let btn1 = document.createElement("button");
 let btn2 = document.createElement("button");
 let btn3 = document.createElement("button");
 let btn4 = document.createElement("button");
 let output = document.createElement("p");
 let scripting = document.createElement("script");
+// setting css
+main.style.fontFamily  = "noto sans mono";
+main.style.textAlign  = "center";
+main.style.  = "";
+main.style.  = "";
+main.style.  = "";
+main.style.  = "";
+main.style.  = "";
+main.style.  = "";
+main.style.  = "";
+main.style.  = "";
+main.style.  = "";
+main.style.  = "";
+main.style.  = "";
+main.style.  = "";
+main.style.  = "";
+headcss.style.fontFamily = "noto sans mono";
+headcss.style.textAlign = "center";
+
