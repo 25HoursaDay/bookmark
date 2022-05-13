555d33da1f1396ae9e3b85e99779c4fa722a930d
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2022-05-13 09:33:37 -0500

    Update something.js

--- something.js
+++ something.js
@@ -53,17 +53,6 @@ function mo(e, c1, c2, s1) {
   e.style.borderColor = c2;
   };
 };
-function s() {
-  color = green;
-  backgroundColor= #000000;
-  fontSize = 20px;
-  borderRadius = 51122px;
-  border = green solid;
-  margin = 10px;
-  position = relative;
-  width = 450px;
-  height = 40px;
-};
 
 function se(e, o) {
   e.id = e;
@@ -79,11 +68,84 @@ mo("btn3", "red", "red", "22px");
 mo("btn4", "red", "red", "22px");
 mo("output", "red", "red", "15px");
 mo("exit", "red", "red", "initial");
-
 se("btn1", "btn1()");
 se("btn2", "btn2()");
 se("btn3", "btn3()");
 se("btn4", "btn4()");
 se("exit", "exit()");
+se("logo", "logo()");
+btn1.style.color = "green";
+btn1.style.backgroundColor = "#000000";
+btn1.style.fontSize = "20px";
+btn1.style.borderRadius = "51122px";
+btn1.style.border = "green solid";
+btn1.style.margin = "10px 10px";
+btn1.style.position = "relative";
+btn1.style.objectPosition = "center 10%"; // x y
+btn1.style.width = "inherit"; // 450px
+btn1.style.height = "40px";
+btn2.style.color = "green";
+btn2.style.backgroundColor = "#000000";
+btn2.style.fontSize = "20px";
+btn2.style.borderRadius = "51122px";
+btn2.style.border = "green solid";
+btn2.style.margin = "10px 10px";
+btn2.style.position = "relative";
+btn2.style.objectPosition = "center 20%"; // x y
+btn2.style.width = "inherit"; // 450px
+btn2.style.height = "40px";
+btn3.style.color = "green";
+btn3.style.backgroundColor = "#000000";
+btn3.style.fontSize = "20px";
+btn3.style.borderRadius = "51122px";
+btn3.style.border = "green solid";
+btn3.style.margin = "10px 10px";
+btn3.style.position = "relative";
+btn3.style.objectPosition = "center 30%"; // x y
+btn3.style.width = "inherit"; // 450px
+btn3.style.height = "40px";
+btn4.style.color = "green";
+btn4.style.backgroundColor = "#000000";
+btn4.style.fontSize = "20px";
+btn4.style.borderRadius = "51122px";
+btn4.style.border = "green solid";
+btn4.style.margin = "10px 10px";
+btn4.style.position = "relative";
+btn4.style.objectPosition = "center 40%"; // x y
+btn4.style.width = "inherit"; // 450px
+btn4.style.height = "40px";
 
-btn1.style.
+exit.style.fontSize = "20px";
+exit.style.backgroundColor = "black";
+exit.style.marginRight = "100%";
+exit.style.marginTop = "100%";
+exit.style. = "";
+exit.style. = "";
+exit.style. = "";
+
+output.style.color = "green";
+output.style.fontSize = "10px"
+
+output.style.display = "none";
+  
+main.style.alignItems = "center";
+
+function exit() {
+main.innerHTML = "";
+};
+  
+function btn1() {
+alert("btn1");
+};
+  
+function btn2() {
+alert("btn2");
+};
+  
+function btn3() {
+alert("btn3");
+};
+  
+function btn4() {
+alert("btn4");
+};
