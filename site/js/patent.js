b2d9498e30a83c7bb3f1f1880337508060ec5698
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2022-09-27 10:56:11 -0500

    Create patent.js

---
+++ site/js/patent.js
@@ -0,0 +1,3 @@
+var lower = 1000000;
+var upper = 9999999;
+window.open('https://patft.uspto.gov/netacgi/nph-Parser?TERM1='+ (Math.floor(Math.random()*(upper-lower))+lower) +'&Sect1=PTO1&Sect2=HITOFF&d=PALL&p=1&u=%2Fnetahtml%2FPTO%2Fsrchnum.htm&r=0&f=S&l=50');
