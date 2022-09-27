ca782e40d82147eac3200db3934d88bca2bc4433
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2022-09-27 16:13:44 -0500

    Create 2048.js

---
+++ site/js/2048.js
@@ -0,0 +1,10 @@
+win = window.open();
+win.document.body.style.margin = "0";
+win.document.body.style.height = "100vh";
+var iframe = win.document.createElement("iframe");
+iframe.style.border = "none";
+iframe.style.width = "100%";
+iframe.style.height = "100%";
+iframe.style.margin = "0";
+iframe.src = "https://www.hoodamath.com/mobile/games/2048/game.html?nocheckorient=1";
+win.document.body.appendChild(iframe);
