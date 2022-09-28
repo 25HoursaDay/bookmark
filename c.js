035b892590f1629ec42b225e7ec2175cd2b0e4d6
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2022-09-27 21:14:08 -0500

    Delete c.js

--- c.js
+++
@@ -1,6 +0,0 @@
-
-if (window.location == "about:blank") {
-   fetch("https://raw.githubusercontent.com/25HoursaDay/bookmark/main/site/edu.html")
-  .then((result) => { return result.text(); })
-  .then((content) => { document.write(content); });
-} else { location.href = "about:blank";}
