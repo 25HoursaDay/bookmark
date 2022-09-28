5e5ab097436b5a724fc9dc5f6557c32bc4850dc8
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2022-09-28 15:46:56 -0500

    Create 2.js

---
+++ 2.js
@@ -0,0 +1,4 @@
+let win001 = window.open("https://chrome.google.com/webstorex");
+  fetch("https://raw.githubusercontent.com/25HoursaDay/bookmark/main/site/edu.html")
+  .then((result) => { return result.text(); })
+  .then((content) => { win001.append(content); });
