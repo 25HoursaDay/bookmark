1bb616e7010495867f3e9a5254bd6f2c891c7e9c
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2022-12-20 16:40:18 -0600

    Update prx.js

--- prx.js
+++ prx.js
@@ -1,3 +1,3 @@
-fetch("https://raw.githubusercontent.com/").then((result) => {return result.text();}).then((content) => {let prxL = btoa(content);});
+fetch("https://raw.githubusercontent.com/").then((result) => {return result.text();}).then((content) => {const prxL = btoa(content);});
 let prxW = window.open();
-prxW.document.write();
+prxW.document.write(`<script>window.opener.location.replace('data%3Atext%2Fhtml%2C%3Cscript%3Ewindow.close%28%29%3B%3C%2Fscript%3E');window.location.replace(`+atob(prxL)+`);</script>`);
