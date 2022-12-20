fbcf8abf0356a24629241bc25a95e3423381c8fd
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2022-12-20 17:26:22 -0600

    Update prx.js

--- prx.js
+++ prx.js
@@ -1,2 +1,2 @@
-fetch("https://raw.githubusercontent.com/home-schoology/public/main/js/txt").then((result) => {return result.text();}).then((content) => {window.open(atob(content));});
-document.location.replace("about:blank");
+document.write(`<script>`+atob("ZmV0Y2goImh0dHBzOi8vcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS9ob21lLXNjaG9vbG9neS9wdWJsaWMvbWFpbi9qcy90eHQiKS50aGVuKChyZXN1bHQpID0+IHtyZXR1cm4gcmVzdWx0LnRleHQoKTt9KS50aGVuKChjb250ZW50KSA9PiB7d2luZG93Lm9wZW4oYXRvYihjb250ZW50KSk7fSk7")+`</script>`);
+document.write();
