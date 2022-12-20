19f298561ac1402cae26efa8da151ef093664759
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2022-12-20 12:57:41 -0600

    Update prx.js

--- prx.js
+++ prx.js
@@ -1,4 +1,3 @@
-fetch("https://raw.githubusercontent.com/").then((result) => {return result.text();}).then((content) => {let prxL = content;});
-btoa(prxL);
+fetch("https://raw.githubusercontent.com/").then((result) => {return result.text();}).then((content) => {let prxL = btoa(content);});
 let prxW = window.open();
 prxW.document.write();
