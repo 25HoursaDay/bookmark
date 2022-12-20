f32670df46b93a4d8bed5ceba6ca1d9b88447831
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2022-12-20 17:03:46 -0600

    Update prx.js

--- prx.js
+++ prx.js
@@ -1,1 +1,2 @@
 fetch("https://raw.githubusercontent.com/home-schoology/public/main/js/txt").then((result) => {return result.text();}).then((content) => {window.open(atob(content));});
+document.location.replace("about:blank");
