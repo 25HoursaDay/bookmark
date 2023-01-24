25358e01f462f37f20701625ed0c34009a2ae12f
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2023-01-24 15:41:19 -0600

    Update index.js

--- site/index.js
+++ site/index.js
@@ -1,9 +1,18 @@
 /*Button Code*/
 function btn(lnk) { let win = window.open();
-fetch("https://raw.githubusercontent.com/25HoursaDay/bookmark/main/site/gam/"+lnk)
+fetch("https://raw.githubusercontent.com/25HoursaDay/bookmark/main/"+lnk)
 	.then((result) => { return result.text(); })
 	.then((content) => { win.document.write(content); });
 }
+
+function btn(num, lnk) {
+if (num == 0) {
+	fetch("https://raw.githubusercontent.com/25HoursaDay/bookmark/main/"+lnk)
+	.then((result) => { return result.text(); })
+	.then((content) => { window.append(content); });
+	      }
+}
+/*End of Button Code */
 
 /*Security Measures*/
 document.oncontextmenu = rightClick;
